ac249edc53293f112c2e808b85a06e258f019359
Brahim <elmerbouh@gmail.com>
2024-12-14 11:22:11 -0500

    updating resources

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq.Expressions;
+using System.Security.Claims;
 using API.Data;
 using API.Data.Repositories.Interfaces;
 using API.DTOs;
@@ -11,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 [Authorize]
-public class UsersController(IUnitOfWork _unitOfWork) : BaseApiController
+public class UsersController(IUnitOfWork _unitOfWork, IMapper _mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -28,5 +29,22 @@ public class UsersController(IUnitOfWork _unitOfWork) : BaseApiController
         if (user == null) return NotFound();
 
         return user;
+    }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in token");
+
+        var user = await _unitOfWork.UserRepository.FindByAsync(u => u.UserName == username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        _mapper.Map(memberUpdateDto, user);
+
+        if (await _unitOfWork.Complete()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
--- clientApp/src/app/_services/members.service.ts
+++ clientApp/src/app/_services/members.service.ts
@@ -1,20 +1,35 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Member } from '../_models/member';
 import { environment } from '../../environments/environment';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http =inject(HttpClient)
+  members = signal<Member[]>([]);
 
   getMembers() {
-    return this.http.get<Member[]>(`${environment.baseApiUrl}users`);
+    return this.http.get<Member[]>(`${environment.baseApiUrl}users`).subscribe({
+      next: members => this.members.set(members)
+    });
   }
 
   gteMember(username: string) {
+    const member = this.members().find(x => x.username === username);
+    if (member !== undefined) return of(member);
+
     return this.http.get<Member>(`${environment.baseApiUrl}users/${username}`);
   }
   
+  updateMember(member:Member){
+    return this.http.put(`${environment.baseApiUrl}users`,member).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => m.username === member.username 
+          ? member : m))
+      })
+    );
+  }
 }
